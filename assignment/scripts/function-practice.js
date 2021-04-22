9553c46552b4e071541bf9af59f85870b58d8c31
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-04-21 20:12:26 -0500

    Questions 9 & 10 complete

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -7,7 +7,7 @@ console.log('***** Function Practice *****');
 // 1. Function to return 'Hello World!'
 function hello() {
   return 'Hello World!';
-}
+} // end function hello
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
 
@@ -17,7 +17,7 @@ let name = 'Dillon';
 
 function helloName() {
   return `Hello, ${name}!`;
-}
+} // end function helloName
 
 // Remember to call the function to test
 
@@ -26,14 +26,14 @@ console.log( helloName() );
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber ) {
   return firstNumber + secondNumber;
-}
+} // end function addNumbers
 
 console.log( addNumbers( 10, 4 ));
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( num1, num2, num3 ){
   return num1 * num2 * num3;
-}
+} // end function multiplyThree
 
 console.log( multiplyThree( 2, 5, 7 ) );
 
@@ -44,8 +44,8 @@ function isPositive( number ) {
     return true;
   } else {
     return false;
-  }
-}
+  } // end if/else statement
+} // end function isPositive
 
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
@@ -66,8 +66,8 @@ function getLast( array ) {
   }
   else {
     return 'undefined';
-  }
-}
+  } // end if/else statement
+} // end function getLast
 
 console.log( getLast(favColors) );
 console.log( getLast(emptyArray) );
@@ -84,12 +84,12 @@ function find( value, array ){
   for (var i = 0; i < array.length; i++) {
     if (value === array[i]) {
       return true;
-    }
-  }
+    } // end if statement
+  } // end for loop
   if (value != array[i]) {
     return false;
-  }
-}
+  } // end if statement
+} // end function find
 
 console.log( find( 'Olive Green', favColors ));
 
@@ -98,24 +98,52 @@ console.log( find( 'Olive Green', favColors ));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
 
-}
+//note-to-self: .charAt() considers the index of the character of the (string
+//in this case).
+
+function isFirstLetter(letter, string) {
+  if (string.charAt(0) === letter) {
+    return true;
+  }
+    else {
+    return false;
+  } // end if/else statement
+} // end function isFirstLetter
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
+
+let someMath = [ 1, 5, 6, 100, 45, 5.3 ];
+
+function sumAll( array ) {
+  let sum = 0;
+    // TODO: loop to add items
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
   return sum;
-}
+} // end function sumAll
+
+console.log(`The sum of the someMath is: ${sumAll( someMath )}` );
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+let inputArray = [ 1, 2, -5, 0, -1.1, 3, -6 ];
 
+function allPositives( array ) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      let outputArray = array.push(i);
+    }
+  } return array;
+}
+
+allPositives( inputArray );
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
